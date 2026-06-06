f5a61720d40625263816fcea913b81f67ead4242
Debabrata Das <debabratadas711@gmail.com>
2026-06-06 17:11:44 +0530

    add new

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,22 +1,43 @@
 import {
   ApplicationConfig,
+  importProvidersFrom,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth-interceptor';
-
+import {
+  LucideAngularModule,
+  Sun,
+  Moon,
+  Menu,
+  X,
+  Trash2,
+  Mail,
+  MapPin,
+  LogOut,
+} from 'lucide-angular';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+        anchorScrolling: 'enabled',
+      }),
+    ),
     provideClientHydration(withEventReplay()),
 
     provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
+
+    importProvidersFrom(
+      LucideAngularModule.pick({ Sun, Moon, Menu, X, Trash2, Mail, MapPin, LogOut }),
+    ),
   ],
 };
