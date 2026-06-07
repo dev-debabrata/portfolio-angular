2f2545a8c921236fbfe5a3175ab949b5e97f1b59
Debabrata Das <debabratadas711@gmail.com>
2026-06-07 20:33:27 +0530

    add new

--- frontend/src/app/admin/components/admin-education/admin-education-list/admin-education-list.ts
+++ frontend/src/app/admin/components/admin-education/admin-education-list/admin-education-list.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin-education-list',
+  standalone: true,
   imports: [],
   templateUrl: './admin-education-list.html',
   styleUrl: './admin-education-list.css',
 })
-export class AdminEducationList {
-
-}
+export class AdminEducationList {}
--- frontend/src/app/admin/pages/profile-admin/profile-admin.ts
+++ frontend/src/app/admin/pages/profile-admin/profile-admin.ts
@@ -4,14 +4,15 @@ import { AdminProfile } from '../../components/admin-profile/admin-profile';
 import { AdminAbout } from '../../components/admin-about/admin-about';
 import { AdminSkillList } from '../../components/admin-skills/admin-skill-list/admin-skill-list';
 import { AdminExperienceList } from '../../components/admin-experience/admin-experience-list/admin-experience-list';
+import { AdminEducationList } from '../../components/admin-education/admin-education-list/admin-education-list';
 
 @Component({
   selector: 'app-profile-admin',
   standalone: true,
-  imports: [AdminProfile, AdminAbout, AdminSkillList, AdminExperienceList],
+  imports: [AdminProfile, AdminAbout, AdminSkillList, AdminExperienceList, AdminEducationList],
   templateUrl: './profile-admin.html',
   styleUrl: './profile-admin.css',
 })
 export class ProfileAdmin {
-  activeTab = signal<'image' | 'about' | 'skills' | 'experience'>('image');
+  activeTab = signal<'image' | 'about' | 'skills' | 'experience' | 'education'>('image');
 }
