cbb0d81b3d2dc684fe939ce08657d0fc76abb170
Debabrata Das <debabratadas711@gmail.com>
2026-06-09 23:19:50 +0530

    education add

--- frontend/src/app/admin/components/admin-education/admin-education-form/admin-education-form.ts
+++ frontend/src/app/admin/components/admin-education/admin-education-form/admin-education-form.ts
@@ -1,11 +1,109 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, effect, inject, input, output, signal } from '@angular/core';
+
+import { EducationForm, EducationResponse } from '../../../../models/education.model';
+import { EducationService } from '../../../../core/services/education.service';
+import { SnackBarService } from '../../../../core/services/snack-bar.service';
 
 @Component({
   selector: 'app-admin-education-form',
+  standalone: true,
   imports: [],
   templateUrl: './admin-education-form.html',
   styleUrl: './admin-education-form.css',
 })
 export class AdminEducationForm {
+  editData = input<EducationResponse | null>(null);
+  closeForm = output<EducationResponse>();
 
+  private EducationService = inject(EducationService);
+  private snackBarService = inject(SnackBarService);
+  private destroyRef = inject(DestroyRef);
+
+  loading = signal(false);
+
+  educationForm = signal<EducationForm>({
+    school: '',
+    degree: '',
+    years: '',
+  });
+
+  constructor() {
+    effect(() => {
+      const edu = this.editData();
+
+      if (edu) {
+        this.educationForm.set({
+          school: edu.school,
+          degree: edu.degree,
+          years: edu.years,
+        });
+      } else {
+        this.educationForm.set({
+          school: '',
+          degree: '',
+          years: '',
+        });
+      }
+    });
+  }
+
+  updateField(field: keyof EducationForm, value: string) {
+    this.educationForm.update((form) => ({
+      ...form,
+      [field]: value,
+    }));
+  }
+
+  submitEducation() {
+    const form = this.educationForm();
+
+    const isSchoolEmpty = !form.school.trim();
+    const isDegreeEmpty = !form.degree.trim();
+    const isYearsEmpty = !form.years.trim();
+
+    if (isSchoolEmpty && isDegreeEmpty && isYearsEmpty) {
+      this.snackBarService.error('All fields are required');
+      return;
+    }
+
+    if (isSchoolEmpty) {
+      this.snackBarService.error('School/College name is required');
+      return;
+    }
+
+    if (isDegreeEmpty) {
+      this.snackBarService.error('Depertment is required');
+      return;
+    }
+
+    if (isYearsEmpty) {
+      this.snackBarService.error('Year is required');
+      return;
+    }
+
+    this.loading.set(true);
+
+    const editEducation = this.editData();
+
+    const educationUpdate = editEducation?._id
+      ? this.EducationService.updateEducation(editEducation._id, form)
+      : this.EducationService.createEducation(form);
+
+    const educationSub = educationUpdate.subscribe({
+      next: (res) => {
+        this.snackBarService.success(res.message);
+        this.loading.set(false);
+        this.closeForm.emit(res.education);
+      },
+
+      error: (err) => {
+        this.snackBarService.error(err.error?.message || 'Education save failed');
+        this.loading.set(false);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      educationSub.unsubscribe();
+    });
+  }
 }
--- frontend/src/app/admin/components/admin-education/admin-education-list/admin-education-list.ts
+++ frontend/src/app/admin/components/admin-education/admin-education-list/admin-education-list.ts
@@ -1,10 +1,89 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { EducationService } from '../../../../core/services/education.service';
+import { SnackBarService } from '../../../../core/services/snack-bar.service';
+import { EducationResponse } from '../../../../models/education.model';
+import { LucideAngularModule } from 'lucide-angular';
+import { AdminEducationForm } from '../admin-education-form/admin-education-form';
 
 @Component({
   selector: 'app-admin-education-list',
   standalone: true,
-  imports: [],
+  imports: [LucideAngularModule, AdminEducationForm],
   templateUrl: './admin-education-list.html',
   styleUrl: './admin-education-list.css',
 })
-export class AdminEducationList {}
+export class AdminEducationList implements OnInit {
+  private educationService = inject(EducationService);
+  private snackBarService = inject(SnackBarService);
+  private destroyRef = inject(DestroyRef);
+
+  educations = signal<EducationResponse[]>([]);
+  editingEducation = signal<EducationResponse | null>(null);
+  showForm = signal(false);
+
+  ngOnInit(): void {
+    const educationSub = this.educationService.getEducation().subscribe({
+      next: (res) => {
+        this.educations.set(res);
+        console.log(res);
+      },
+
+      error: (err) => {
+        console.log(err.message);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      educationSub.unsubscribe();
+    });
+  }
+
+  deleteEducation(id: string) {
+    const confirmed = confirm('Are you sure delete this Education');
+
+    if (!confirmed) {
+      return;
+    }
+
+    const eduSub = this.educationService.deleteEducation(id).subscribe({
+      next: (res) => {
+        this.educations.update((educations) =>
+          educations.filter((education) => education._id !== id),
+        );
+        this.snackBarService.success(res.message);
+      },
+
+      error: (err) => {
+        this.snackBarService.error(err.message);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      eduSub.unsubscribe();
+    });
+  }
+
+  openForm(education?: EducationResponse) {
+    this.editingEducation.set(education || null);
+    this.showForm.set(true);
+  }
+
+  closeForm(saveEducation?: EducationResponse) {
+    this.showForm.set(false);
+
+    if (!saveEducation) {
+      this.editingEducation.set(null);
+      return;
+    }
+
+    if (this.editingEducation()) {
+      this.educations.update((items) =>
+        items.map((item) => (item._id === saveEducation._id ? saveEducation : item)),
+      );
+    } else {
+      this.educations.update((items) => [saveEducation, ...items]);
+    }
+
+    this.editingEducation.set(null);
+  }
+}
