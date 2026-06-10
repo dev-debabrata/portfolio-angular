74a6228cf45634854a418ee6c94e4f343329e70d
Debabrata Das <debabratadas711@gmail.com>
2026-06-10 22:08:11 +0530

    add new

--- frontend/src/app/admin/components/admin-experience/admin-experience-form/admin-experience-form.ts
+++ frontend/src/app/admin/components/admin-experience/admin-experience-form/admin-experience-form.ts
@@ -1,11 +1,107 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, effect, inject, input, output, signal } from '@angular/core';
+import { ExperienceService } from '../../../../core/services/experience.service';
+import { SnackBarService } from '../../../../core/services/snack-bar.service';
+import { ExperienceForm, ExperienceResponse } from '../../../../models/experience.model';
 
 @Component({
   selector: 'app-admin-experience-form',
+  standalone: true,
   imports: [],
   templateUrl: './admin-experience-form.html',
   styleUrl: './admin-experience-form.css',
 })
 export class AdminExperienceForm {
+  editData = input<ExperienceResponse | null>(null);
+  closeForm = output<ExperienceResponse>();
 
+  private experienceService = inject(ExperienceService);
+  private snackBarService = inject(SnackBarService);
+  private destroyRef = inject(DestroyRef);
+
+  loading = signal(false);
+
+  experienceForm = signal<ExperienceForm>({
+    company: '',
+    role: '',
+    years: '',
+  });
+
+  constructor() {
+    effect(() => {
+      const exp = this.editData();
+
+      if (exp) {
+        this.experienceForm.set({
+          company: exp.company,
+          role: exp.role,
+          years: exp.years,
+        });
+      } else {
+        this.experienceForm.set({
+          company: '',
+          role: '',
+          years: '',
+        });
+      }
+    });
+  }
+
+  updateField(field: keyof ExperienceForm, value: string) {
+    this.experienceForm.update((form) => ({
+      ...form,
+      [field]: value,
+    }));
+  }
+
+  submitExperience() {
+    const form = this.experienceForm();
+
+    const isCompanyEmpty = !form.company.trim();
+    const isRoleEmpty = !form.role.trim();
+    const isYearsEmpty = !form.years.trim();
+
+    if (isCompanyEmpty && isRoleEmpty && isYearsEmpty) {
+      this.snackBarService.error('All fields are required');
+      return;
+    }
+
+    if (isCompanyEmpty) {
+      this.snackBarService.error('Company name is required');
+      return;
+    }
+
+    if (isRoleEmpty) {
+      this.snackBarService.error('Role is required');
+      return;
+    }
+
+    if (isYearsEmpty) {
+      this.snackBarService.error('Years is required');
+      return;
+    }
+
+    this.loading.set(true);
+
+    const editExperience = this.editData();
+
+    const experienceUpdate = editExperience?._id
+      ? this.experienceService.updateExperience(editExperience._id, form)
+      : this.experienceService.createExperience(form);
+
+    const experienceSub = experienceUpdate.subscribe({
+      next: (res) => {
+        this.snackBarService.success(res.message);
+        this.loading.set(false);
+        this.closeForm.emit(res.experience);
+      },
+      error: (err) => {
+        this.snackBarService.error(err.error?.message || 'Experience save failed');
+        this.loading.set(false);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      experienceSub.unsubscribe();
+    });
+  }
 }
--- frontend/src/app/admin/components/admin-experience/admin-experience-list/admin-experience-list.ts
+++ frontend/src/app/admin/components/admin-experience/admin-experience-list/admin-experience-list.ts
@@ -1,11 +1,99 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { ExperienceService } from '../../../../core/services/experience.service';
+import { SnackBarService } from '../../../../core/services/snack-bar.service';
+import { ExperienceResponse } from '../../../../models/experience.model';
+import { LucideAngularModule } from 'lucide-angular';
+import { AdminExperienceForm } from '../admin-experience-form/admin-experience-form';
 
 @Component({
   selector: 'app-admin-experience-list',
-  imports: [],
+  standalone: true,
+  imports: [LucideAngularModule, AdminExperienceForm],
   templateUrl: './admin-experience-list.html',
   styleUrl: './admin-experience-list.css',
 })
-export class AdminExperienceList {
+export class AdminExperienceList implements OnInit {
+  private experienceService = inject(ExperienceService);
+  private destroyRef = inject(DestroyRef);
+  private snackBarService = inject(SnackBarService);
 
+  experiences = signal<ExperienceResponse[]>([]);
+  editingExperience = signal<ExperienceResponse | null>(null);
+  showForm = signal(false);
+
+  ngOnInit(): void {
+    const experienceSub = this.experienceService.getExperiences().subscribe({
+      next: (res) => {
+        this.experiences.set(res);
+        console.log(res);
+      },
+
+      error: (err) => {
+        console.log(err.message);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      experienceSub.unsubscribe();
+    });
+  }
+
+  deleteExperience(id: string) {
+    const confirmed = confirm('Are you sure delete this Experience?');
+
+    if (!confirmed) {
+      return;
+    }
+
+    const exSub = this.experienceService.deleteExperience(id).subscribe({
+      next: (res) => {
+        this.experiences.update((experiences) =>
+          experiences.filter((experience) => experience._id !== id),
+        );
+        this.snackBarService.success(res.message);
+      },
+      error: (err) => {
+        this.snackBarService.error(err.message);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      exSub.unsubscribe();
+    });
+  }
+
+  openForm(experience?: ExperienceResponse) {
+    this.editingExperience.set(experience || null);
+    this.showForm.set(true);
+  }
+
+  closeForm(savedExperience?: ExperienceResponse) {
+    this.showForm.set(false);
+
+    if (!savedExperience) {
+      this.editingExperience.set(null);
+      return;
+    }
+
+    if (this.editingExperience()) {
+      this.experiences.update((items) =>
+        items.map((item) => (item._id === savedExperience._id ? savedExperience : item)),
+      );
+    } else {
+      this.experiences.update((items) => [savedExperience, ...items]);
+    }
+
+    this.editingExperience.set(null);
+  }
+
+  // openForm() {
+  //   this.showForm.set(true);
+  // }
+
+  // closeForm(newExperience?: ExperienceResponse) {
+  //   this.showForm.set(false);
+  //   if (newExperience) {
+  //     this.experiences.update((experiences) => [...experiences, newExperience]);
+  //   }
+  // }
 }
